1eb120a4aa621978ca0d54d08f0902e067eeb217
darylldan <dccaponpon@up.edu.ph>
2026-04-29 15:01:29 +0800

    feat: changed adapter from mariadb C connector to pymysql

--- sql/sql_connector.py
+++ sql/sql_connector.py
@@ -1,5 +1,5 @@
-import mariadb
 import sys
+import pymysql
 
 '''
 This file creates a designated class to talk to the MariaDB interface. To avoid overloading the database
@@ -11,21 +11,21 @@ Pre-requisites:
 '''
 
 class MariaDBInstance():
-    conn = None
-    cursor: mariadb.Cursor = None
+    conn: pymysql.Connection
+    cursor: pymysql.cursors.Cursor 
 
     # Initialization of the MariaDB connection, on the constructor of the class
     # host and port have default values, meaning that if left out when creating the instance of the class, the default values will be set
     def __init__(self, user: str, password: str, database: str, host: str = "127.0.0.1", port : int = 3306):
         try: 
-            self.conn = mariadb.connect(
+            self.conn = pymysql.connect(
                 user = user,
                 password = password,
                 host = host,
                 port = port,
                 database = database
             )
-        except mariadb.Error as e:
+        except pymysql.Error as e:
             print(f"Error connecting to MariaDB Platform: {e}")
             sys.exit(1)
 
@@ -33,7 +33,7 @@ class MariaDBInstance():
         self.cursor = self.conn.cursor()
 
     # Cursor object getter
-    def cur(self) -> mariadb.Cursor:
+    def cur(self) -> pymysql.cursors.Cursor:
         return self.cursor
     
     # Closes the connection, cursor will no longer be accessible after this
